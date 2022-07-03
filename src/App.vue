4fd493d5459fd959265926fa09cdefa2bcff0bb9
CFCFM <1812357607@qq.com>
2022-07-03 14:31:38 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>app组件</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style scoped lang="less"></style>
